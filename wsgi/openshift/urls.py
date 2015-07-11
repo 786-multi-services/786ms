e635db57a160be3096a6532ee698e845cfef6d4a
zkhan93 <zkhan1093@gmail.com>
2015-07-11 16:08:56 +0530

    corrected

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+ url(r'^',include('home.urls')),
+  url(r'^career/',include('career.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
