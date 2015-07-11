5b4162ec8b4ac1390df7f683aae2184130836d56
zkhan93 <zkhan1093@gmail.com>
2015-07-11 15:38:31 +0530

    sfsd

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
 
 setup(name='YourAppName',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='786 Multi Services',
+      author='zeeshan',
+      author_email='zkhan1093@example.com',
       url='https://pypi.python.org/pypi',
       install_requires=packages,
 )
