245d210d710c918048db45ef9a5141800ae5cb21
Zeeshan Khan <zkhan1093@gmail.com>
2016-07-06 17:16:03 +0530

    decouples students and shops app

--- wsgi/openshift/shops/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-urlpatterns = [
-	url(r'^$',views.listShops,name="listShops"),
-    url(r'^(?P<shop_id>[\d+])$',views.shopSummary,name="ShopSummary"),
-    url(r'^(?P<shop_id>[\d+])/(?P<cycle>[\w]+)$',views.shopSummary,name="ShopSummary"),
-    url(r'^(?P<shop_id>[\d+])/(?P<cycle>[\w]+)/(?P<delta>[\d]+)$',views.shopSummary,name="ShopSummary"),
-]
