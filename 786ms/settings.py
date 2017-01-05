d3be69ee078692d997ff9106b4914c64b4582f9a
Zeeshan Khan <zkhan1093@gmail.com>
2017-01-05 14:50:18 +0530

    added new domain in debug ips

--- 786ms/settings.py
+++ 786ms/settings.py
@@ -41,7 +41,7 @@ else:
 # TEMPLATE_DEBUG = DEBUG
 
 if DEBUG:
-     ALLOWED_HOSTS = ['multi-services-786.herokuapp.com','localhost']
+     ALLOWED_HOSTS = ['786ms.baangeeclub.in','multi-services-786.herokuapp.com','localhost']
 else:
      ALLOWED_HOSTS = ['*']
 
